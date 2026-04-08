b2f7ce99d862fbab4548ae6ff0c1d7002c8b47db
exppii <5418088+tjuliuyou@users.noreply.github.com>
2026-04-08 15:27:04 +0800

    feat: add quick-lock on app background and iOS keychain cleanup

--- app/lib/page/stealth/stealth_home.dart
+++ app/lib/page/stealth/stealth_home.dart
@@ -3,10 +3,36 @@ import 'package:omnigram/l10n/generated/L10n.dart';
 import 'package:omnigram/page/stealth/stealth_library_page.dart';
 import 'package:omnigram/theme/colors.dart';
 
-class StealthHome extends StatelessWidget {
+class StealthHome extends StatefulWidget {
   final String encryptionKey;
 
   const StealthHome({super.key, required this.encryptionKey});
+
+  @override
+  State<StealthHome> createState() => _StealthHomeState();
+}
+
+class _StealthHomeState extends State<StealthHome>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused ||
+        state == AppLifecycleState.inactive) {
+      Navigator.of(context).pop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +48,7 @@ class StealthHome extends StatelessWidget {
           ),
         ],
       ),
-      body: StealthLibraryPage(encryptionKey: encryptionKey),
+      body: StealthLibraryPage(encryptionKey: widget.encryptionKey),
     );
   }
 }
